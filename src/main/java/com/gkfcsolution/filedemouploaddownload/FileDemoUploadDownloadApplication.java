28da518d2cf2af127204b57f386d8d91c7a28fe8
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-12-09 16:49:25 +0100

    first commit

---
+++ src/main/java/com/gkfcsolution/filedemouploaddownload/FileDemoUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.filedemouploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDemoUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileDemoUploadDownloadApplication.class, args);
+    }
+
+}
